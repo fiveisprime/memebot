4d6fe80b3b0c3bdd49c0c01b4c7c1259ad33d5bf
Matt Hernandez <matt@modulus.io>
2013-12-23 13:24:28 -0500

    Added unit tests.

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ SRC = index.js lib/memebot.js
 test: $(SRC)
 	@node node_modules/.bin/jshint $^ \
 	--verbose
+	@node node_modules/.bin/mocha \
+	--require should \
+	--reporter spec \
+	spec
---
+++ spec/memebot-spec.js
@@ -0,0 +1,37 @@
+var nock    = require('nock')
+  , EE      = require('events').EventEmitter
+  , memebot = require('../lib/memebot');
+
+var client = new EE()
+  , bot    = memebot(client);
+
+describe('meme', function() {
+
+  it('should say a meme to the correct channel on `!meme`', function(done) {
+    nock('http://api.automeme.net')
+      .get('/text?lines=1')
+      .reply(200, 'test');
+
+    client.say = function(channel, meme) {
+      channel.should.equal('#channel');
+      meme.should.equal('test');
+      done();
+    };
+
+    client.emit('message', 'user', '#channel', '!meme');
+  });
+
+  it('should handle private messages', function(done) {
+    nock('http://api.automeme.net')
+      .get('/text?lines=1')
+      .reply(200, 'pm');
+
+    client.say = function(from, meme) {
+      from.should.equal('user');
+      meme.should.equal('pm');
+      done();
+    };
+
+    client.emit('pm', 'user');
+  });
+});
